148d82c130a0e47f583d1d7d860f15c8c486e02e
Sandeep Maram <msrt20@gmail.com>
2017-05-15 23:57:04 +0530

    port scan added

--- app/src/main/java/com/msr/dnsdemo/activities/SecondActivity.java
+++ app/src/main/java/com/msr/dnsdemo/activities/SecondActivity.java
@@ -26,16 +26,41 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener, IOnDeviceDetected {
     ArrayList<ClientScanResult> deviceInfoArrayList = new ArrayList();
     private TextView textView1;
     private SecondAdapter adapter = null;
     private ProgressBar pBar;
+
+    public static Future<Boolean> portIsOpen(final ExecutorService es, final String ip, final int port, final int timeout) {
+        return es.submit(new Callable<Boolean>() {
+            @Override
+            public Boolean call() {
+                try {
+                    Socket socket = new Socket();
+                    socket.connect(new InetSocketAddress(ip, port), timeout);
+                    socket.close();
+                    Log.i("===Port", "==Open::" + port);
+                    return true;
+                } catch (Exception ex) {
+                    return false;
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +77,7 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         CommonMethods.getCurrentSsid(this);
         CommonMethods.getCurrentSsid1(this);
         CommonMethods.getWifiInfo(this);
-
+        test();
         // scan();
 
     }
@@ -74,6 +99,33 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
                 startActivity(new Intent(this, ClientActivity.class));
                 break;
         }
+    }
+
+    public void test() {
+
+        final ExecutorService es = Executors.newFixedThreadPool(20);
+        final String ip = "192.168.1.14";
+        final int timeout = 200;
+        final List<Future<Boolean>> futures = new ArrayList<>();
+        for (int port = 1; port <= 65535; port++) {
+            futures.add(portIsOpen(es, ip, port, timeout));
+        }
+        es.shutdown();
+        int openPorts = 0;
+        for (final Future<Boolean> f : futures) {
+            try {
+                if (f.get()) {
+                    openPorts++;
+                    break;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.i("===IP:::", "====Ports" + openPorts);
+        System.out.println("There are " + openPorts + " open ports on host " + ip + " (probed with a timeout of " + timeout + "ms)");
     }
 
     @Override
